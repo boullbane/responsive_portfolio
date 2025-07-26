8532c1a80cd25d052fe2795bbd80a05deac65b1b
BOULBAN Hicham <boullban.hicham@gmail.com>
2025-07-26 22:32:22 +0100

    add some javascript effect

---
+++ dist/JS/about_script.js
@@ -0,0 +1,20 @@
+const text = "Let me tell a few things about me...";
+const typingText = document.getElementById("typing-text");
+const cursor = document.querySelector(".cursor");
+
+let i = 0;
+
+function typeWriter() {
+    if (i < text.length) {
+        typingText.textContent += text.charAt(i);
+        i++;
+        setTimeout(typeWriter, 100); // Adjust speed here
+    }
+    else {
+        setTimeout(() => {
+            cursor.style.display = "none";
+        }, 2000);
+    }
+}
+
+window.onload = typeWriter;
